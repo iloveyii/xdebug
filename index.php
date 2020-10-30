f2593291958b530a9b38fd07f45cf8f30f24fce4
Hazrat Ali <iloveyii@yahoo.com>
2020-10-30 18:28:04 +0100

    Added readme

--- index.php
+++ index.php
@@ -6,31 +6,31 @@
  */
 
 
-/**
- * 2.
- * Create a vh debug.loc using vh
- * Install xdebug if not already : sudo apt-get install php-xdebug
- * Verify php -m
- * Change port to 9000 in /etc/php/7.4/apache2/conf.d/20-xdebug.ini, restart apache2
- * Click Run > Edit configurations
- * Add web app, with server created in vh above
- * Click validate to validate config
- *
- * contents of /etc/php/7.4/apache2/conf.d/20-xdebug.ini:
- */
+    /**
+     * 2.
+     * Create a vh debug.loc using vh
+     * Install xdebug if not already : sudo apt-get install php-xdebug
+     * Verify php -m
+     * Change port to 9000 in /etc/php/7.4/apache2/conf.d/20-xdebug.ini, restart apache2
+     * Click Run > Edit configurations
+     * Add web app, with server created in vh above
+     * Click validate to validate config
+     *
+     * contents of /etc/php/7.4/apache2/conf.d/20-xdebug.ini:
+     */
 
-/**
+    /**
 
-zend_extension=xdebug.so
-xdebug.show_error_trace = 1
-zend_extension=/usr/lib/php/20190902/xdebug.so
-xdebug.remote_autostart = 1
-xdebug.remote_enable = 1
-xdebug.remote_handler = dbgp
-xdebug.remote_host = 127.0.0.1
-xdebug.remote_log = /tmp/xdebug_remote.log
-xdebug.remote_mode = req
-xdebug.remote_port = 9000
+    zend_extension=xdebug.so
+    xdebug.show_error_trace = 1
+    zend_extension=/usr/lib/php/20190902/xdebug.so
+    xdebug.remote_autostart = 1
+    xdebug.remote_enable = 1
+    xdebug.remote_handler = dbgp
+    xdebug.remote_host = 127.0.0.1
+    xdebug.remote_log = /tmp/xdebug_remote.log
+    xdebug.remote_mode = req
+    xdebug.remote_port = 9000
 
 
  */
